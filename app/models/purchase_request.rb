b92ca8b967d89ca0ead46550e79f2ddb4b1e35f4
AnjanaShree30 <saravanananjanashree@gmail.com>
2026-07-17 23:54:44 +0530

    Day 13 exercise: Log status changes in after_save callback

---
+++ app/models/purchase_request.rb
@@ -0,0 +1,46 @@
+class PurchaseRequest < ApplicationRecord
+  belongs_to :user
+
+  enum :status, {
+    draft: 0,
+    submitted: 1,
+    approved: 2,
+    rejected: 3
+  }
+
+  validates :title, presence: true
+
+  validates :amount,
+            presence: true,
+            numericality: { greater_than: 0 }
+
+  before_validation :set_default_status
+
+  after_create :log_creation
+  after_save :notify_on_status_change
+  after_commit :log_commit, on: [:create, :update]
+
+  private
+
+  def set_default_status
+    self.status ||= :draft
+  end
+
+  def log_creation
+    Rails.logger.info "PurchaseRequest ##{id} created: #{title}"
+  end
+
+  def notify_on_status_change
+  if saved_change_to_status?
+    old_status, new_status = saved_change_to_status
+
+    Rails.logger.info(
+      "PurchaseRequest ##{id} status changed from #{old_status} to #{new_status}"
+    )
+  end
+end
+
+  def log_commit
+    Rails.logger.info "PurchaseRequest ##{id} committed to DB"
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :purchase_requests, dependent: :destroy
+
   enum :role, {
     requester: 0,
     approver: 1,
